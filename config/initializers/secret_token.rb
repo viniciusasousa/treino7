5f1b36e892ace2e0d8cb0132a148133e00210a56
Vinicius Sousa <viniciusasousa@yahoo.com.br>
2010-12-18 19:35:03 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treino7::Application.config.secret_token = '6a972a52dd444bf9097d0274795402ceadd7180b10c5dbc6d61d0f70fda9ef0579c885d101dcca7d6dad6e7e1fc124ad953ea46b456271a6b247f3c6898f7fb3'
